411152873a1b3e545b62550a2155ed9a943a7434
jkuang-nj <179047250@qq.com>
2018-11-29 21:49:35 +0800

    update

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/QStarDB.cpp 
+../src/reversedb.cpp 
 
 OBJS += \
-./src/QStarDB.o 
+./src/reversedb.o 
 
 CPP_DEPS += \
-./src/QStarDB.d 
+./src/reversedb.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
